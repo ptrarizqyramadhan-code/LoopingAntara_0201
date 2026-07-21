e0b5410bec50b8613016227b2dc349a6787a088c
ptrarizqyramadhan-code <ptrarizqyramadhan@gmail.com>
2026-07-21 10:38:35 +0700

    perulangan dengan for turun

--- Loopingfor.cpp
+++ Loopingfor.cpp
@@ -10,3 +10,13 @@ int main() {
         cout << "Nilai i : " << i << " Teknologi Informasi" << endl;
     }
     cout << endl;
+
+// perulangan dengan  FOR untuk turun 
+    cout << "Perulangan  FOR untuk turun" << endl;
+    for (int i = 5; i >= 1; i--) {
+        cout << "Nilai i : " << i << " Teknologi Informasi UMY " << endl;
+    }
+    cout << endl;
+
+    return 0;
+}
