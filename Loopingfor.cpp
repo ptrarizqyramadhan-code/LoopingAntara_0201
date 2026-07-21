570d7175b9ddb803376339d8c49546b2008cbc74
ptrarizqyramadhan-code <ptrarizqyramadhan@gmail.com>
2026-07-21 10:37:44 +0700

    perulangan dengan for naik

--- Loopingfor.cpp
+++ Loopingfor.cpp
@@ -4,3 +4,9 @@ using namespace std;
 int main() {
     //perulangan dengan FOR
     cout << "Perulangan  FOR" << endl;
+
+    //perulangan dengan FOR untuk naik
+    for (int i = 1; i <= 5; i++) {
+        cout << "Nilai i : " << i << " Teknologi Informasi" << endl;
+    }
+    cout << endl;
