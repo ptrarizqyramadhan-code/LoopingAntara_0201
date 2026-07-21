4c075304ddb7d19ba01605e7c2db0b8ae66240ca
ptrarizqyramadhan-code <ptrarizqyramadhan@gmail.com>
2026-07-21 11:09:48 +0700

    syart syarat

--- LoopingDoWhile.cpp
+++ LoopingDoWhile.cpp
@@ -12,3 +12,14 @@ int main() {
     cout << "Perulangan  DO WHILE" << endl;
     // Nilai awal x = 1
     x = 1 + rand() % 10;
+
+  
+    do {
+        cout << "Ti, Bilangan Acak = " << x << endl;
+        x =  rand() % 10; // pengubah dari nilai x
+    } while(x <= 5); // syarat
+
+    cout << "bilangan acak do-while yang terakhir = " << x << endl;
+    cout << endl;
+    return 0;
+}
