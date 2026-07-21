26b56e13e1b59ff7254c5b4153646d9233ce331a
ptrarizqyramadhan-code <ptrarizqyramadhan@gmail.com>
2026-07-21 11:29:57 +0700

    bilangan acak while yang terakhir

--- LoopingWhile.cpp
+++ LoopingWhile.cpp
@@ -10,3 +10,13 @@ int main() {
     
     cout << "Perulangan  WHILE" << endl;
     x = 1 + rand() % 10;
+
+    while(x <= 5) {
+        cout << "Ti, Bilangan Acak = " << x << endl;
+        x =  rand() % 10;
+    }
+    cout << "bilangan acak while yang terakhir = " << x << endl;
+    cout << endl;
+
+    return 0;
+}
