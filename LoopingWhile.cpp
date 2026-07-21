6416d7e112eb0f95f78e461e1c26dcadead3b0eb
ptrarizqyramadhan-code <ptrarizqyramadhan@gmail.com>
2026-07-21 11:26:32 +0700

    libbry while

--- LoopingWhile.cpp
+++ LoopingWhile.cpp
@@ -2,21 +2,3 @@
 #include <cstdlib>
 #include <ctime>
 using namespace std;
-
-int main() {
-    int x;
-
-    srand(time(0));
-    
-    cout << "Perulangan  WHILE" << endl;
-    x = 1 + rand() % 10;
-
-    while(x <= 5) {
-        cout << "Ti, Bilangan Acak = " << x << endl;
-        x =  rand() % 10;
-    }
-    cout << "bilangan acak while yang terakhir = " << x << endl;
-    cout << endl;
-
-    return 0;
-}
